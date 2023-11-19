4644443741ea384489c1b2f4556b5e753e42cf3e
Harsh Sharma <harshsharmaktm03@gmail.com>
2023-11-18 20:10:53 -0800

    Day2 work inbuild node modules checks 18nov 2k23

---
+++ S1/Day2/index.js
@@ -0,0 +1,6 @@
+// in this file i am using a internal node modules to check a number it it even or not
+//  if i found a even number isEven function will return me a boolen value "true" else it 
+//  will return me "false".
+
+var isEven = require('is-even');
+console.log(isEven(18));
